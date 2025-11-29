96f3056aaa580c1e0803c5eb7c8cc12ee5bafbde
AnguyenTf <anguyen57329@gmail.com>
2025-11-28 19:03:48 -0800

    Uploaded module 8, OS

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// Modules
+const names = require('./4-names')
+const sayHi = require('./5-ultis')
+const data = require('./6-alternative-flavor')
+require('./7-mind-ger')
+
+console.log(names)
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.doge)
